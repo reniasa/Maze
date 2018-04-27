187defa473baefd3fed36aeaf7c3199659283c2c
Renata Ciesielska <reniasac@gmail.com>
2018-04-28 00:40:48 +0200

    Add new project, initial maze scene and basic player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed;
+    public GameObject deatParticles;
+
+    private float _maxSpeed = 5f;
+    private Vector3 _input;
+    private Vector3 _spawnPosition;
+    private Rigidbody _rigidbody;
+
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody>();
+        _spawnPosition = transform.position;
+	}
+	
+	void Update () {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if(_rigidbody.velocity.magnitude < _maxSpeed)
+        {
+            _rigidbody.AddForce(_input * moveSpeed);
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Enemy")
+        {
+            Instantiate(deatParticles, transform.position, Quaternion.identity);
+            transform.position = _spawnPosition;
+        }
+    }
+}
